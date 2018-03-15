534148458c9b0f248222c912926493809ae4fb52
Lewis Wilcock <lewis.wilcock@citypantry.com>
2018-03-15 12:14:40 +0000

    change name of function to report instead of hello, add a sample json file for testing (will remove later and do properly), start implementing proper reporting

--- backend/handlers/slack-report.ts
+++ backend/handlers/slack-report.ts
@@ -1,13 +1,24 @@
 import { HandlerRequest, HandlerResponse } from 'serverless-api-handlers';
 import { slackApi } from '../api/slack.api';
 
-export async function hello(request: HandlerRequest): Promise<HandlerResponse> {
+export async function report(request: HandlerRequest): Promise<HandlerResponse> {
 
-  slackApi.post("Hello, world!")
+  const lines: string[] = [];
+
+  const params = request.body; // as BugReport;
+
+  console.log(params);
+    
+  for (let i = 0;  i < params.length; i++) {
+    lines.push(i + ". " + params[i]);
+  }
+
+  slackApi
+    .post(lines.join('\n'))
     .catch(reason => console.log(reason));
 
   return {
     statusCode: 200,
-    body: '"HIYA"',
+    body: '"Invoked successfully!"',
   };
 }
