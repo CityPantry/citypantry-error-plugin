dfd5f1568c59a5508c799b54161a42a37eee564d
Lewis Wilcock <lewis.wilcock@citypantry.com>
2018-03-16 13:44:38 +0000

    add remaining sample configuration properties

--- config/config.sample.ts
+++ config/config.sample.ts
@@ -5,5 +5,7 @@ export const config = {
   jiraUser: 'user@email.com',
   jiraToken: 'token from https://id.atlassian.com/manage/api-tokens',
   jiraServer: 'https://your-organisation.atlassian.net',
-  oauthToken: 'Generate your oauth token'
+  oauthToken: 'Generate your oauth token',
+  projectKey: 'The project key is the prefix that your stories have e.g. ABC-123 has project key ABC',
+  bugIssueName: 'Most likely "Bug" or similar. ',
 };
