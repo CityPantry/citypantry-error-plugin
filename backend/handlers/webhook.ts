88c8f631631bb598d24b93616287ab0f071c821a
paullessing <paul@paullessing.com>
2021-01-13 12:45:17 +0000

    Style fixes & cleanup

--- backend/handlers/webhook.ts
+++ backend/handlers/webhook.ts
@@ -6,7 +6,7 @@ import { BlockData, slackApi } from '../api/slack.api';
 export const main: APIGatewayProxyHandler = async (apiEvent) => {
   const event: JiraIssueEvent = JSON.parse(apiEvent.body);
 
-  await processEvent(event); // Run asynchronously; return immediately
+  await processEvent(event);
 
   return {
     statusCode: 201,
@@ -15,27 +15,22 @@ export const main: APIGatewayProxyHandler = async (apiEvent) => {
 }
 
 async function processEvent(event: JiraIssueEvent): Promise<void> {
-  console.log('Webhook event: ', JSON.stringify(event, null, 2));
 
-  let isCreateEvent = isCreate(event);
+  const isCreateEvent = event.webhookEvent === 'jira:issue_created';
   const createdWithDevTeamField = isCreateEvent && !!event.issue.fields.customfield_10922;
-  if (!createdWithDevTeamField &&
-    !event.changelog.items.some((item) => item.fieldId === 'customfield_10922')
-  ) {
+  const devTeamFieldWasUpdated = event.changelog.items.some((item) => item.fieldId === 'customfield_10922');
+
+  if (!createdWithDevTeamField && !devTeamFieldWasUpdated) {
     return;
   }
 
   const previousValue = isCreateEvent ? null : event.changelog.items.find((item) => item.fieldId === 'customfield_10922').from as string | null;
-  const newValue = event.issue.fields.customfield_10922;
-
   const previousTeams = previousValue ? findTeamsByChangelogValue(previousValue) : [];
+
+  const newValue = event.issue.fields.customfield_10922;
   const newTeams = newValue ? newValue.map(({id}) => Teams.get(id)).filter(Boolean) : [];
 
-  console.log('Dev Team Updated', previousValue, previousTeams, newValue, newTeams);
-
   const teamsAdded = newTeams.filter(({id}) => !previousTeams.find(({id: previousId}) => `${previousId}` === `${id}`));
-
-  console.log('New Teams:', teamsAdded);
 
   if (!teamsAdded.length) {
     return;
@@ -54,21 +49,11 @@ async function processEvent(event: JiraIssueEvent): Promise<void> {
       userName: event.user.displayName,
     });
 
-    // const postText = `Bug <${issueKey}|${issueLink}> was assigned to ${team.name} team by ${event.user.displayName}`;
-    // const description = (event.issue.fields.description || '').trim();
-    // const attachments: any[] = [{
-    //   'fallback': `Bug ${issueKey}`,
-    //   'title': `${issueKey}: ${event.issue.fields.summary}`,
-    //   'title_link': issueLink,
-    //   'text': `${description.length > 300 ? description.substr(0, 300).trimRight() + '…' : description}${description ? '\n\n' : ''}<View Issue|${issueLink}>`,
-    // }];
-
     try {
       await slackApi.post({
         blocks,
         channel: team.slackChannel,
         username: 'Bug Monitoring',
-        // attachments,
       });
     } catch (e) {
       console.log('ERROR', e);
@@ -86,21 +71,6 @@ function findTeamsByChangelogValue(changelogValue: string): TeamConfig[] {
   }
 }
 
-function isCreate(event: JiraIssueEvent): boolean {
-  return event.webhookEvent === 'jira:issue_created';
-}
-/*
-    {
-      "field": "Dev Team",
-      "fieldtype": "custom",
-      "fieldId": "customfield_10922",
-      "from": "[10244]",
-      "fromString": "Supply",
-      "to": "[10218, 10244]",
-      "toString": "Fulfilment,Supply"
-    }
-*/
-
 function generateBlocks(data: { userName: string, teamName: string, issueLink: string, issueKey: string, issueTitle: string, issueDescription: string }): BlockData[] {
   const MAX_LINES = 5;
   const MAX_CHARACTERS = 300;
@@ -110,7 +80,7 @@ function generateBlocks(data: { userName: string, teamName: string, issueLink: s
   const lines = description.split('\n');
   const lineBreakIndex = lines.slice(0, MAX_LINES).map((line) => line.length + 1).reduce((a, b) => a + b, 0) - 1;
   const breakPoint = Math.min(description.length, lineBreakIndex, MAX_CHARACTERS);
-  const shortDescription = description.substr(0, breakPoint) + (breakPoint <= description.length ? '…' : '');
+  const shortDescription = description ? description.substr(0, breakPoint) + (breakPoint <= description.length ? '…' : '') : '';
 
   return [
     {
@@ -147,20 +117,5 @@ function generateBlocks(data: { userName: string, teamName: string, issueLink: s
     {
       type: 'divider'
     },
-    // {
-    //   type: 'actions',
-    //   elements: [
-    //     {
-    //       type: 'button',
-    //       text: {
-    //         type: 'plain_text',
-    //         text: 'View Bug',
-    //         emoji: true
-    //       },
-    //       value: 'open_bug',
-    //       url: data.issueLink
-    //     }
-    //   ]
-    // }
   ].filter(Boolean);
 }
