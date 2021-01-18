7b43a2cc0b068bbcfc765401621b83a8d8b6da37
paullessing <paul@paullessing.com>
2021-01-18 09:03:58 +0000

    Test endpoint does the same as report

--- backend/handlers/test.ts
+++ backend/handlers/test.ts
@@ -1,14 +1,13 @@
-import { IncidentSize, Report, toHumanString } from '@models';
+import { Report, toHumanString } from '@models';
 import { APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda';
-import axios from 'axios';
 import * as uuid from 'uuid/v4';
 import { config } from '../../config';
 import { awsApi } from '../api/aws.api';
+import { Bug, Document as Doc, jiraApi } from '../api/jira.api';
 import { BlockData, BlocksPostData, slackApi } from '../api/slack.api';
-import { image } from './image';
 
-export const main: APIGatewayProxyHandler = async () => {
-  const response = await report(undefined, undefined, undefined);
+export const main: APIGatewayProxyHandler = async (event) => {
+  const response = await report(event, undefined, undefined);
   if (response) {
     if (!response.headers) {
       response.headers = {};
@@ -18,54 +17,100 @@ export const main: APIGatewayProxyHandler = async () => {
   return response as APIGatewayProxyResult;
 }
 
-const report: APIGatewayProxyHandler = async () => {
+const report: APIGatewayProxyHandler = async (event) => {
+  const body: Report = (() => {
+    if (typeof event.body === 'object') {
+      return event.body;
+    } else {
+      try {
+        return JSON.parse(event.body);
+      } catch (e) {
+        console.log('Failed to JSON parse:', body);
+        throw e;
+      }
+    }
+  })();
 
-  const report: Report = {
-    name: 'Reporter Name',
-    email: '',
-    summary: 'Bacon ipsum dolor amet spare ribs prosciutto biltong, chuck tenderloin frankfurter ribeye sausage drumstick cow.',
-    description: 'Bacon ipsum dolor amet spare ribs prosciutto biltong,\nchuck tenderloin frankfurter ribeye sausage drumstick cow.\nTenderloin chislic strip steak pastrami, corned beef frankfurter pork belly turkey biltong.\nHamburger ham spare ribs prosciutto sausage, capicola rump cupim pork tenderloin ribeye andouille venison landjaeger.\n\nTurducken biltong chuck pork, chicken short loin ribeye jerky jowl tongue cow.',
-    screenshot: null,
-    url: 'https://google.com',
-    isMasquerading: true,
-    stepsToReproduce: '1. Do thing\n2. Do another thing\n3. Don\'t do the next thing',
-    incidentSize: IncidentSize.MEDIUM,
-    consoleErrors: '',
-    currentUser: 'Current User',
-    affectedPeople: 'Just Me',
-    time: new Date().toISOString(),
-  };
+  const parsedReport = body as Report & { impact: string };
+  const report = {
+    ...parsedReport,
+    name: trim(parsedReport.name),
+    summary: trim(parsedReport.impact || parsedReport.summary),
+    description: trim(parsedReport.description),
+    affectedPeople: trim(parsedReport.affectedPeople),
+    stepsToReproduce: trim(parsedReport.stepsToReproduce),
+    currentUser: trim(parsedReport.currentUser)
+  } as Report;
 
-  const screenshot = Buffer.from(image.replace(/^data:image\/\w+;base64,/, ''), 'base64');
+  // SLACK
+  const screenshot = report.screenshot ? Buffer.from(report.screenshot.replace(/^data:image\/\w+;base64,/, ''), 'base64') : null;
 
   const reportUuid = uuid(); // used in filenames so that they are not guessable by the public
-  const imageName = `screenshot-${report.name.replace(/\W/gi, '').toLowerCase()}-TEST-${reportUuid}.png`;
-  const slackId = '';
+  const imageName = `screenshot-${report.name.replace(/\W/gi, '').toLowerCase()}-${report.time}-${reportUuid}.png`;
+  const errorFileName = `console-errors-${report.name.replace(/\W/gi, '').toLowerCase()}-${report.time}-${reportUuid}.json`;
+  let dataUrl: string | null = null;
+  if (report.consoleErrors && report.consoleErrors.length > 1000) {
+    console.log(`consoleErrors is too long (${report.consoleErrors.length}, uploading`);
+    dataUrl = await awsApi.uploadText(report.consoleErrors, errorFileName);
+    console.log(`Uploaded consoleErrors to ${dataUrl}`);
+  }
+  const slackId = await getSlackId(report.email);
+
+  const imageUrl = screenshot ? await awsApi.uploadImage(screenshot, imageName) : null;
+  if (imageUrl) {
+    console.log(`Uploaded image to ${imageUrl}`);
+  }
+
+  // JIRA
+  const bug: Bug = {
+    incidentSize: report.incidentSize,
+    summary: report.summary,
+    description: createJiraDescription(report, imageUrl, dataUrl)
+  };
 
   try {
-    const issueKey = 'CPD-00000'
+    const issueKey = await jiraApi.createIssue(bug);
+
     const { post: slackPost, threadReply } = createSlackBody(report, slackId, issueKey);
     console.log('Posting', slackPost);
-    const { permalink: slackUrl, ts: slackTs, channel: slackChannel } = await retry(() => slackApi.post({ ...slackPost, channel: '#slack-test' }))
-    console.log('Updating:', {
-      channel: slackChannel,
-      thread_ts: slackTs,
-      blocks: threadReply
-    });
-    await slackApi.uploadImage(slackChannel, slackTs, imageName, screenshot);
+    const channel = '#slack-test' || config.channel;
+    const { permalink: slackUrl, ts: slackTs, channel: slackChannel } = await slackApi.post({ ...slackPost, channel });
+    // console.log('Updating:', {
+    //   channel: slackChannel,
+    //   thread_ts: slackTs,
+    //   blocks: threadReply
+    // });
     await slackApi.post({ // Thread reply
       channel: slackChannel,
       threadTs: slackTs,
       blocks: threadReply
     });
+    if (screenshot) {
+      try {
+        await slackApi.uploadImage({
+          data: screenshot,
+          channels: slackChannel,
+          threadTs: slackTs,
+          filename: 'Screenshot for ' + issueKey
+        });
+      } catch (e) {
+        console.log('Error uploading screenshot', e);
+        await slackApi.post({
+          channel: slackChannel,
+          threadTs: slackTs,
+          text: '*Screenshot:* ' + imageUrl
+        })
+      }
+    }
 
-    console.log('Slack URL', slackUrl);
-
+    if (slackUrl) {
+      await jiraApi.updateIssueDescription(issueKey, updateDescriptionWithSlackLink(bug, slackUrl));
+    }
   } catch (error) {
     console.log('Upstream error', error);
     return {
       statusCode: 503,
-      body: JSON.stringify(error.message),
+      body: JSON.stringify(error),
     }
   }
 
@@ -74,6 +119,24 @@ const report: APIGatewayProxyHandler = async () => {
     body: '"Invoked successfully!"',
   };
 }
+
+async function getSlackId(email: string): Promise<string | null> {
+  let slackId = null;
+  try {
+    const user = await slackApi.findUserByEmail(email);
+    if (user) {
+      slackId = user.id;
+    }
+  } catch (e) {
+    console.log('Failed to retrieve Slack user:', e);
+  }
+  return slackId;
+}
+
+function trim(text: string): string {
+  return (text || '').trim();
+}
+
 
 function createSlackBody(report: Report, slackId: string | null, issueKey: string): { post: BlocksPostData, threadReply: BlockData[] } {
   const jiraLink = `${config.jiraServer}/browse/${issueKey}`;
@@ -101,7 +164,7 @@ function createSlackBody(report: Report, slackId: string | null, issueKey: strin
           type: 'section',
           text: {
             type: 'mrkdwn',
-            text: `*URL:* <${report.url}|https://citypantry.com/foo/bar?baasdaasda>`
+            text: `*URL:* <${report.url}|${report.url}>`
           }
         },
         {
@@ -137,99 +200,35 @@ ${report.stepsToReproduce}`
       }
     ],
   };
-
-/*
-  const attachments: any[] = [{
-    'fallback': `Bug report ${issueKey} reported by ${report.name} for ${report.time} at ${report.url}`,
-    'title': `${issueKey}: ${report.summary}`,
-    'title_link': `${config.jiraServer}/browse/${issueKey}`,
-    'text': `*Reporter:* ${report.name}${slackId ? ` (<@${slackId}>)` : ''}
-*Incident Size*: ${toHumanString(report.incidentSize)}
-
-*What's Wrong?*
-${report.description}
-
-*Time:* ${report.time}
-*Affected People:* ${report.affectedPeople}
-
-*Steps to Reproduce*:
-_${report.isMasquerading ? 'Masquerading' : 'Logged in'} as ${report.currentUser}_
-${report.url}
-
-${report.stepsToReproduce}
-
-*Issue URL*
-${config.jiraServer}/browse/${issueKey}`,
-    'color': '#FF8000'
-  }];
-  if (imageUrl) {
-    attachments.push({
-      'fallback': `Screenshot uploaded by ${report.name} for ${report.time} at ${imageUrl}`,
-      'title': `Screenshot of ${issueKey}`,
-      'title_link': imageUrl,
-      'image_url': imageUrl,
-      'color': '#FF8000'
-    })
-  }
-  return attachments;
- */
 }
 
-function retry<T>(callback: () => Promise<T>, retryDelayMs: number = 100, maxTimeoutMs: number = 5000): Promise<T> {
-  return new Promise<T>(async (resolve, reject) => {
-    let isResolved = false;
-    setTimeout(() => {
-      if (isResolved) {
-        return;
-      }
-      console.log('Aborting waiting');
-      isResolved = true;
-      reject(new Error(`Timed out after ${maxTimeoutMs}ms.`));
-    }, maxTimeoutMs);
-
-    while (!isResolved) {
-      console.log('Trying');
-      try {
-        const response = await callback();
-        if (isResolved) {
-          return;
-        }
-
-        isResolved = true;
-        resolve(response);
-      } catch (e) {
-        console.log(`Failed, delaying retry by ${retryDelayMs}ms...`, e);
-
-        await new Promise((resolve) => setTimeout(resolve, retryDelayMs));
-      }
-    }
-  });
+function createJiraDescription(report: Report, screenshotUrl: string | null, dataUrl: string | null): Bug['description'] {
+  return {
+    version: 1,
+    type: 'doc',
+    content: [
+      Doc.p(Doc.text(report.description)),
+      Doc.p(
+        Doc.text('User: ', 'strong'), Doc.text((report.isMasquerading ? 'Masquerading as ' : '') + report.currentUser), Doc.br,
+        Doc.text('URL: ', 'strong'), Doc.link(report.url), Doc.br,
+        Doc.text('Time: ', 'strong'), Doc.text(report.time),
+      ),
+      Doc.p(
+        Doc.text('Steps to Reproduce:', 'strong'), Doc.br,
+        Doc.text(report.stepsToReproduce)
+      ),
+      Doc.p(Doc.text('Affected People: ', 'strong'), Doc.text(report.affectedPeople)),
+      Doc.p(Doc.text('Number of Affected People: ', 'strong'), Doc.text(toHumanString(report.incidentSize))),
+      Doc.p(Doc.text('Screenshot:', 'strong'), Doc.br, screenshotUrl ? Doc.link(screenshotUrl) : Doc.text('No Screenshot')),
+      Doc.p(Doc.text('Console data:', 'strong'), Doc.br, dataUrl ? Doc.link(dataUrl) : Doc.text(Buffer.from(report.consoleErrors || '<no data>').toString('base64'))),
+      Doc.p(Doc.text('Reported By: ', 'strong'), Doc.text(report.name)),
+    ]
+  };
 }
 
-// function waitUntilImageIsAvailable(imageUrl: string): Promise<boolean> {
-//   return new Promise<boolean>(async (resolve) => {
-//     let isResolved = false;
-//     setTimeout(() => {
-//       if (isResolved) {
-//         return;
-//       }
-//       console.log('Aborting waiting');
-//       isResolved = true;
-//       resolve(false);
-//     }, 5000);
-//
-//     while (!isResolved) {
-//       console.log('Trying', imageUrl);
-//       const response = await axios.head(imageUrl);
-//       if (isResolved) {
-//         return;
-//       }
-//       console.log('Response is', response.status);
-//       if (response.status >= 200 && response.status < 300) {
-//         isResolved = true;
-//         resolve(true);
-//       }
-//       await new Promise((resolve) => setTimeout(resolve, 100));
-//     }
-//   });
-// }
+function updateDescriptionWithSlackLink(bug: Bug, slackUrl: string): Bug['description']['content'] {
+  return [
+    ...bug.description.content,
+    Doc.p(Doc.text('Slack Link: ', 'strong'), Doc.link(slackUrl)),
+  ];
+}
